c02bcf22c2acf60f38c97d19de9c9af980d88c80
OrrenJ <ojey11@gmail.com>
2017-04-28 17:29:26 -0500

    Changes for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "dyrutiy786futiugyd5s7dfugy"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:iyfmq0j007i@localhost/project2"
+# local
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:iyfmq0j007i@localhost/project2"
+# heroku
+app.config['SQLALCHEMY_DATABASE_URL'] = "postgresql://gtuhxuesxaerwz:8927799e7c3a60d6fb68ccb0ff2a059bf532378f7137a1eb463e1a8ee4271117@ec2-54-243-252-91.compute-1.amazonaws.com:5432/d8u15687avuqtg"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
